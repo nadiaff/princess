666c8de4b4c7b56eafebf394c38841a6c9e40222
Nadia <nadiaff@gmail.com>
2016-12-20 22:17:03 -0500

    Setting margins and height size for iframe in em

--- pong.php
+++ pong.php
@@ -28,8 +28,8 @@
 		<!-- End Navigation -->
 		<!-- End Top Bar -->
 
-		<div class="columns row">
-			<iframe style="height:500px; width:500px;" src="games/pong-game.html"></iframe>
+		<div class="columns row medium-6">
+			<iframe style="height:33em; width:33em; margin-top:5em; margin-bottom:5em;" src="games/pong-game.html"></iframe>
 		</div>
 		<!-- Start Footer -->
 		<?php include 'php/footer.php'; ?>
